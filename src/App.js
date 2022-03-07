596cce91ac06931a0b76f31d8c45ba00c9b607aa
Ankit Kumar Mishra <ankit5kumar14@gmail.com>
2022-03-07 20:32:45 +0530

    Hooks - useState and useEffect

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TableComponent from "./components/TableComponent";
+import { Container, Button, ProgressBar } from "react-bootstrap";
 
 function App() {
+  const [show, setShow] = useState(true);
+  const [count, setCount] = useState(0);
+
+  // Replicating componentDidMount using useEffect hook
+  useEffect(() => {
+    console.log('Component Mounted!')
+  }, [])
+
+  // Replicating componentDidUpdate using useEffect hook
+  useEffect(() => {
+    console.log('Count or Show state was updated')
+  }, [count, show])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App">
+      {show && <TableComponent />}
+      <Button variant="dark" onClick={() => setShow(!show)}>
+        {show ? 'Hide' : 'Show'}
+      </Button>
+      <ProgressBar now={count} />
+      <Button variant="dark" onClick={() => setCount(count + 1)}>
+        Increment
+      </Button>
+    </Container>
   );
 }
 
